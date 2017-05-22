5b684f3430f4993a3d392b4f9f44fc533345f783
rengarajan <rengarajan.net@gmail.com>
2017-05-22 16:24:22 +0530

    Angular

---
+++ renga/src/Identity/Startup.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using IdentityModel;
+using System.Security.Claims;
+using IdentityServer4;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Identity
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+            // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddIdentityServer()
+.AddInMemoryClients(Clients.Get())
+.AddInMemoryIdentityResources(Resources.GetIdentityResources())
+.AddInMemoryApiResources(Resources.GetApiResources())
+.AddTestUsers(Users.Get())
+.AddTemporarySigningCredential();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseIdentityServer();
+            app.UseApplicationInsightsRequestTelemetry();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseApplicationInsightsExceptionTelemetry();
+
+            // middleware for google authentication
+            app.UseGoogleAuthentication(new GoogleOptions
+            {
+                AuthenticationScheme = "Google",
+                SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+                ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com",
+                ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh"
+            });
+
+            // middleware for external openid connect authentication
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
+                SignOutScheme = IdentityServerConstants.SignoutScheme,
+
+                DisplayName = "OpenID Connect",
+                Authority = "https://demo.identityserver.io/",
+                ClientId = "implicit",
+
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = "role"
+                }
+            });
+
+            app.UseStaticFiles();
+
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            var clients = new List<Client> {
+            new Client {
+                ClientId = "oauthClient",
+                ClientName = "Example Client Credentials Client Application",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = new List<Secret> {
+                    new Secret("superSecretPassword".Sha256())},
+                AllowedScopes = new List<string> {"customAPI.read"}
+            }
+        };
+            clients.Add(new Client
+            {
+                ClientId = "openIdConnectClient",
+                ClientName = "Example Implicit Client Application",
+                AllowedGrantTypes = GrantTypes.Implicit,
+                AllowedScopes = new List<string>
+                                    {
+                                        IdentityServerConstants.StandardScopes.OpenId,
+                                        IdentityServerConstants.StandardScopes.Profile,
+                                        IdentityServerConstants.StandardScopes.Email,
+                                        "role",
+                                        "customAPI.write"
+                                    },
+                RedirectUris = new List<string> { "http://localhost:3000/signin-oidc" },
+                PostLogoutRedirectUris = new List<string> { "http://localhost:3000" }
+            });
+            return clients;
+        }
+    }
+
+    internal class Resources
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource> {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email(),
+            new IdentityResource {
+                Name = "role",
+                UserClaims = new List<string> {"role"}
+            }
+        };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource> {
+            new ApiResource {
+                Name = "customAPI",
+                DisplayName = "Custom API",
+                Description = "Custom API Access",
+                UserClaims = new List<string> {"role"},
+                ApiSecrets = new List<Secret> {new Secret("scopeSecret".Sha256())},
+                Scopes = new List<Scope> {
+                    new Scope("customAPI.read"),
+                    new Scope("customAPI.write")
+                }
+            }
+        };
+        }
+    }
+
+
+    internal class Users
+    {
+        public static List<TestUser> Get()
+        {
+            return new List<TestUser> {
+            new TestUser {
+                SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                Username = "scott",
+                Password = "password",
+                Claims = new List<Claim> {
+                    new Claim(JwtClaimTypes.Email, "scott@scottbrady91.com"),
+                    new Claim(JwtClaimTypes.Role, "admin")
+                }
+            }
+        };
+        }
+    }
+
+}
